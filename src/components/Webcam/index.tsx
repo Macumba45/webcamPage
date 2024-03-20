858e7f311a6471cf0ef56a1c2278e912f21c2651
Macumba45 <gonzalolovo@gmail.com>
2024-03-20 20:01:37 +0100

    styles: new watermark

--- src/components/Webcam/index.tsx
+++ src/components/Webcam/index.tsx
@@ -185,7 +185,9 @@ const WebcamComponent: FC = () => {
                     style={{
                         display: 'flex',
                         justifyContent: 'center',
-                        alignItems: 'center',
+                        position: 'absolute',
+                        width: '100%',
+                        height: '100%',
                     }}
                 >
                     <div
